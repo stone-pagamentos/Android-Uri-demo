b5f4caf272d20fee46e67e6a9d12c86ed3e59a73
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-02-21 13:09:05 -0300

    uploading default application

---
+++ app/src/main/java/br/com/stone/uri/MainActivity.java
@@ -0,0 +1,9 @@
+package br.com.stone.uri;
+
+import com.jgabrielfreitas.core.activity.BaseActivity;
+import com.jgabrielfreitas.layoutid.annotations.InjectLayout;
+
+@InjectLayout(layout = R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+}
