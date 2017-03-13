d36f9e97472c69bd0f1d8a1c9fdf45f889b53891
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-03-13 16:46:04 -0300

    hotfix #4

--- app/src/main/java/br/com/stone/uri/adapter/OptionsViewHolder.java
+++ app/src/main/java/br/com/stone/uri/adapter/OptionsViewHolder.java
@@ -1,6 +1,7 @@
 package br.com.stone.uri.adapter;
 
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import br.com.stone.uri.R;
 import br.com.stone.uri.listeners.OptionsListener;
@@ -16,7 +17,8 @@ import uk.co.ribot.easyadapter.annotations.ViewId;
 @LayoutId(R.layout.option_view_holder_row)
 public class OptionsViewHolder extends ItemViewHolder<String> {
 
-  @ViewId(R.id.optionTextView) TextView optionTextView;
+  @ViewId(R.id.optionTextView)         TextView       optionTextView;
+  @ViewId(R.id.rootCellRelativeLayout) RelativeLayout rootCellRelativeLayout;
 
   public OptionsViewHolder(View view) {
     super(view);
@@ -27,9 +29,9 @@ public class OptionsViewHolder extends ItemViewHolder<String> {
   }
 
   @Override public void onSetValues(final String item, PositionInfo positionInfo) {
-    optionTextView.setText(item);
 
-    optionTextView.setOnClickListener(new View.OnClickListener() {
+    optionTextView.setText(item);
+    rootCellRelativeLayout.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
 
         OptionsListener listener = getListener(OptionsListener.class);
